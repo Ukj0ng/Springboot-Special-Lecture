973c9e7aa7cd969f014f4688c27f6d21dce3b092
Ukjong <xb253q@gmail.com>
2024-05-08 17:39:01 +0900

    feat: User 구현

--- src/main/java/com/uk/person/domain/User/User.java
+++ src/main/java/com/uk/person/domain/User/User.java
@@ -1,2 +1,17 @@
-package com.uk.person.domain.User;public class User {
+package com.uk.person.domain.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {     // 얘는 여러 객체를 필요로 할 수 있어서 IoC컨테이너에서 관리하게 하면 안됨
+    private int id;
+    private String username;
+    private String password;
+    private String phone;
 }
