723065180409213fa42bb60053c72b430f857c32
Ukjong <xb253q@gmail.com>
2024-05-09 11:53:37 +0900

    feat: exception 처리를 위한 handler 구현

--- src/main/java/com/uk/person/config/GlobalExceptionHandler.java
+++ src/main/java/com/uk/person/config/GlobalExceptionHandler.java
@@ -1,2 +1,16 @@
-package com.uk.person.config;public class ExceptionHandler {
+package com.uk.person.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice   // global exception을 관리
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = IllegalAccessException.class)
+    public String 요청잘못(IllegalAccessException e) {
+        System.out.println(e.getMessage());
+        return "오류" + e.getMessage();
+    }
 }
