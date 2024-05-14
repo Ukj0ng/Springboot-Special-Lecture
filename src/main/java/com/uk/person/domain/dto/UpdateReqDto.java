5bbdcf1814f9246b8888e99b7e1cbfd4df210705
Ukjong <xb253q@gmail.com>
2024-05-14 15:00:41 +0900

    feat: password에 어노테이션 추가 - @NotBlank

--- src/main/java/com/uk/person/domain/dto/UpdateReqDto.java
+++ src/main/java/com/uk/person/domain/dto/UpdateReqDto.java
@@ -1,9 +1,11 @@
 package com.uk.person.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdateReqDto {
+    @NotBlank(message = "password를 입력하세요")
     private String password;
     private String phone;
 }
